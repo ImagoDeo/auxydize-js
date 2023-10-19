4eb24a384704b059308b52e86169c3365b5dbc3b
Joshua Kieffer <kieffer.joshua.k@gmail.com>
2023-10-19 11:53:54 -0500

    chore: improve base32.js
    When I say 'improve', I mean it uses less lines of code now. It's not, strictly speaking, a better implementation. It just so happens that Javascript numbers can have a radix inserted for their toString() function call, and padding that is pretty easy too, so here we are: 11 lines to decode, 11 lines to encode.

--- src/base32.js
+++ src/base32.js
@@ -8,58 +8,29 @@ const lib = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
 
 function encode(bytes, pad = true) {
   let bits = '';
-
   for (const uint8 of bytes) {
-    let val = uint8;
-    bits += [128, 64, 32, 16, 8, 4, 2, 1].reduce((prev, current) => {
-      if (val >= current) {
-        val -= current;
-        prev += '1';
-      } else {
-        prev += '0';
-      }
-      return prev;
-    }, '');
+    bits += uint8.toString(2).padStart(8, '0');
   }
-
   if (bits.length % 5) bits += '0'.repeat(5 - (bits.length % 5));
   let str = '';
-
   for (let c = 5; c <= bits.length; c += 5) {
     str += lib[Number('0b' + bits.slice(c - 5, c))];
   }
-
   if (pad && str.length % 8) str += '='.repeat(8 - (str.length % 8));
-
   return str;
 }
 
 function decode(string) {
   const str = string.replace(/=/g, '').toUpperCase();
   let bits = '';
-
   for (const char of str) {
-    let val = lib.indexOf(char);
-    bits += [16, 8, 4, 2, 1].reduce((prev, current) => {
-      if (val >= current) {
-        val -= current;
-        prev += '1';
-      } else {
-        prev += '0';
-      }
-      return prev;
-    }, '');
+    bits += lib.indexOf(char).toString(2).padStart(5, '0');
   }
-
-  // Trim extra zeroes added for padding.
   if (bits.length % 8) bits = bits.slice(0, -(bits.length % 8));
-
   let buffer = Buffer.alloc(bits.length / 8);
-
   for (let c = 8; c <= bits.length; c += 8) {
     buffer.writeUint8(Number('0b' + bits.slice(c - 8, c)), (c - 8) / 8);
   }
-
   return buffer;
 }
 
